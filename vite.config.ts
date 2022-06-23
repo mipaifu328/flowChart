e48e6d9b930b24a82dc3d9d9d5549e6ed7845045
mipaifu328 <594757223@qq.com>
2022-06-23 14:43:49 +0800

    fix:base

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'flowChart/live/',
+  base: 'flowChart/',
   plugins: [
     vue({
       reactivityTransform: true,
